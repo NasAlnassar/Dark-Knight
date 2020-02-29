5418de541b19e6c15c2985992050158928db442b
NasAlnassar <61634563+NasAlnassar@users.noreply.github.com>
2020-02-29 17:43:02 -0500

    Add files via upload

---
+++ IR_Sensor_Test.ino
@@ -0,0 +1,23 @@
+#define sensor1 A0
+#define sensor2 A1
+#define sensor3 A2
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); // start the serial port
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // 5v
+  float volts1 = analogRead(sensor1)*0.0048828125;  // value from sensor * (5/1024)
+  Serial.print("Sensor 1: ");
+  Serial.println(volts1);
+  float volts2 = analogRead(sensor2)*0.0048828125;  // value from sensor * (5/1024)
+  Serial.print("Sensor 2: ");
+  Serial.println(volts2);
+  float volts3 = analogRead(sensor3)*0.0048828125;  // value from sensor * (5/1024)
+  Serial.print("Sensor 3: ");
+  Serial.println(volts3);
+  delay(3000);
+}
